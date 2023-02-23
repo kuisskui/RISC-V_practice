63fe0f4454b63d1f375780c53e2388b70fc68fc2
kuisskui <kuikaopong.2002@gmail.com>
2023-02-23 22:36:18 +0700

    done and comment

--- example1.s
+++ example1.s
@@ -3,25 +3,25 @@ i: .word 0
 n: .word 6
 
 .text
-main:   la      t0, i
-        lw      t0, 0(t0)
-        la      t1, n
-        lw      t1, 0(t1)
-        addi    t2, x0, 1
-checkWhile:  bne     t1, t2, while
-        addi    a0, x0, 1
-        addi    a1, t0, 0
-        ecall
-        addi    a0, x0, 10
-        ecall
-while:  addi    t0, t0, 1
-        addi    t3, x0, 2
-        rem     t4, t1, t3
-        beq     t4, x0, if
-        addi    t5, x0, 3
-        mul     t1, t1, t5
-        addi    t1, t1, 1
-        j       checkWhile
-if:  div    t1, t1, t3
-        j       checkWhile
+main:   la      t0, i                   # int *t0 = &i;
+        lw      t0, 0(t0)               # t0 = *t0;
+        la      t1, n                   # int *t1 = &n;
+        lw      t1, 0(t1)               # t1 = *t1;
+        addi    t2, x0, 1               # t2 = 1;
+checkWhile:  bne     t1, t2, while      # if(t1 != t2) goto while
+        addi    a0, x0, 1               # a0 = 1;
+        addi    a1, t0, 0               # a1 = t0;
+        ecall                           # printf("%d", t0);
+        addi    a0, x0, 10              # a0 = 10;
+        ecall                           # terminate
+while:  addi    t0, t0, 1               # t0 = t0 + 1;
+        addi    t3, x0, 2               # t3 = 2;
+        rem     t4, t1, t3              # t4 = t1%t3;
+        beq     t4, x0, if              # if(t4 == 0) goto if
+        addi    t5, x0, 3               # t5 = 3;
+        mul     t1, t1, t5              # t1 = t1*t5;
+        addi    t1, t1, 1               # t1 = t1 + 1;
+        j       checkWhile              # goto checkWhile
+if:  div    t1, t1, t3                  # t1 = t1 / t3;
+        j       checkWhile              # goto checkWhile
 
--- example2.s
+++ example2.s
@@ -1,6 +1,6 @@
 .data
 n: .word 6079
-is_prime .word 0
+is_prime: .word 0
 k: .word 0
 
 .text
@@ -12,20 +12,20 @@ main:   la      t0, n           # int *t0 = &n;
         lw      t2, 0(t2)       # t2 = *t2;
         addi    t3, x0, 1       # t3 = 1
         beq     t0, t3, funcIf  # if(t0==t3) goto funcIf
-        addi    t1, x0, 1       
-        addi    t2, x0, 2
-checkwhile: blt t2, t0, while
-while:  rem     t4, t0, t2
-        beq     t4, x0, if-while
-        addi    t2, t2, 1
-        j       checkwhile
-if-while:   
-        addi    t1, x0, 0
-        j       exit
-funcIf: addi    t1, x0, 0
-        j       exit
-exit:   addi    a0, x0, 1
-        addi    a1, t1, 0
-        ecall
-        addi    a0, x0, 10
-        ecall
+        addi    t1, x0, 1       # t1 = 1;
+        addi    t2, x0, 2       # t2 = 2;
+checkwhile: blt t2, t0, while   # if(t2<t0) goto while
+        bge     t2, t0, exit    # if(t2>=t0) goto exit
+while:  rem     t4, t0, t2      # t4 = t0%t2;
+        beq     t4, x0, if-while# if(t4==0) goto if-while
+        addi    t2, t2, 1       # t2 = t2+1
+        j       checkwhile      # goto checkwhile
+if-while:   addi    t1, x0, 0   # t1 = 0;
+        j       exit            # goto exit
+funcIf: addi    t1, x0, 0       # t1 = 0;
+        j       exit            # goto exit
+exit:   addi    a0, x0, 1       # a0 = 1;
+        addi    a1, t1, 0       # a1 = t1;
+        ecall                   # printf("%d", t1);
+        addi    a0, x0, 10      # a0 = 10;
+        ecall                   # terminate
